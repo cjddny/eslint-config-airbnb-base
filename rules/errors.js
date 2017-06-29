baa967e406fdd2a6eee5f7a3970392adcad42923
chenjingduo <chenjingduo@corp.netease.com>
2017-06-29 19:35:39 +0800

    off no-console

--- rules/errors.js
+++ rules/errors.js
@@ -22,7 +22,7 @@ module.exports = {
     'no-cond-assign': ['error', 'always'],
 
     // disallow use of console
-    'no-console': 'warn',
+    'no-console': 'off',
 
     // disallow use of constant expressions in conditions
     'no-constant-condition': 'warn',
